b231ae9aca0276b80df6ecde8ae4ca845111f43b
Afonso_Dolmen <quadradoplay@outlook.com>
2021-01-18 20:07:47 -0200

    Line line intersection

---
+++ js/line.js
@@ -0,0 +1,40 @@
+class Line{
+    constructor(x1,y1,x2,y2){
+        this.pos = createVector(x1,y1);
+        this.dir = createVector(x2,y2);
+    }
+
+    draw(){
+        line(this.pos.x,this.pos.y,
+            this.dir.x,this.dir.y);
+        stroke(255);
+    }
+
+    lookAt(){
+        this.mouse = createVector(mouseX,mouseY);
+        this.look = p5.Vector.sub(this.mouse,this.dir);
+        this.dir.add(this.look);
+    }
+
+    collision(collisor){
+        this.denominator = ((this.dir.x - this.pos.x) *
+        (collisor.dir.y - collisor.pos.y)) -
+        ((this.dir.y - this.pos.y) * 
+        (collisor.dir.x - collisor.pos.x));
+
+        this.numerator1 = ((this.pos.y - collisor.pos.y) *
+                            (collisor.dir.x - collisor.pos.x)) -
+                            ((this.pos.x - collisor.pos.x) *
+                            (collisor.dir.y - collisor.pos.y));
+        
+        this.numerator2 = ((this.pos.y - collisor.pos.y) *
+                            (this.dir.x - this.pos.x)) -
+                            ((this.pos.x - collisor.pos.x) *
+                            (this.dir.y - this.pos.y));
+        
+        this.r = this.numerator1 / this.denominator;
+        this.s = this.numerator2 / this.denominator;
+
+        return (this.r >= 0 && this.r <= 1) && (this.s >= 0 && this.s <= 1);
+    }
+}
---
+++ js/sketch.js
@@ -0,0 +1,22 @@
+let ray;
+let surface;
+
+function setup(){
+    createCanvas(400,400);
+
+    ray = new Line(100,200,300,200);
+    surface = new Line(300,100,300,300);
+}
+
+function draw(){
+    background(0);
+
+    ray.draw();
+    ray.lookAt();
+
+    surface.draw();
+
+    if(ray.collision(surface)){
+        stroke(255,0,0);
+    }
+}
